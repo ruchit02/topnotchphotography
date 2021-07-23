1a519af769e704aa11d67ea89e89dc76145f3803
Ruchit Darji <darjiruchit02@gmail.com>
2021-07-23 16:50:57 +0530

    Initial website launch

--- assets/js/get-image-dimensions.js
+++ assets/js/get-image-dimensions.js
@@ -1,58 +1,19 @@
-let images   = document.getElementsByClassName( "actual-img" );
-let imgLinks = document.getElementsByClassName( "img-link" );
-let imgTexts = document.getElementsByClassName( "img-text" );
-let imgViews = document.getElementsByClassName( "view-button" ); 
+let images   = document.getElementsByClassName( "actual-image" );
+let galleryLinks = document.getElementsByClassName( "gallery-link" );
+let locations = document.getElementsByClassName( "location" );
+let names = document.getElementsByClassName( "name" ); 
 
-let index = 0;
+for( let i = 0; i < galleryLinks.length ; ++i ){
 
-for( let img of images ){
+    galleryLinks[i].onmouseover = function(){
 
-    img.onload = function(){
-
-        let imgW = img.naturalWidth;
-        let imgH = img.naturalHeight;
-        
-        let ratio = imgW/imgH ;
-    
-        let vh = 0.8*window.innerHeight;
-    
-        let sampleH = imgH;
-        while( sampleH > vh ){
-    
-            sampleH = sampleH-2;
-        }
-    
-        let sampleW = Math.floor( ratio*sampleH );
-    
-        img.height = sampleH;
-        img.width  = sampleW;
-
-        if( (index % 2) === 0 ){
-            imgLinks[index].style.float = "right";
-        }else{
-            imgLinks[index].style.float = "left";
-            imgTexts[index].style.right = 0;
-            imgTexts[index].style.textAlign = "right";
-
-            imgViews[index].style.right = 0;
-        }
-
-        ++index;
-    }
-}
-
-
-for( let i = 0; i < 4 ; ++i ){
-
-    imgLinks[i].onmouseover = function(){
-
-        imgTexts[i].className = imgTexts[i].className + " responsive" ;
-        imgViews[i].className = imgViews[i].className + " responsive" ;
+        locations[i].className = locations[i].className + " responsive" ;
+        names[i].className = names[i].className + " responsive" ;
     }
 
-    imgLinks[i].onmouseout = function(){
+    galleryLinks[i].onmouseout = function(){
 
-        imgTexts[i].className = "img-text" ;
-        imgViews[i].className = "view-button" ;
+        locations[i].className = "location" ;
+        names[i].className = "name" ;
     }
 }
