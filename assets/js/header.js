95617c3c9c76371213f822a128d3812adeea7fbe
Ruchit Darji <darjiruchit02@gmail.com>
2021-07-26 10:16:18 +0530

    Initial website launch

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,6 +1,8 @@
+let header         = document.getElementById( "pageHeader" );
 let navBar         = document.getElementById( "navigation" );
 let headerLinksBox = document.getElementById( "sideMenuOpen" );
 let closeBtn       = document.getElementById( "sideMenuClose" );
+let prevScrollY    = window.pageYOffset;
 
 navBar.onclick = function(){
     headerLinksBox.style.width = "45%";
@@ -10,3 +12,15 @@ closeBtn.onclick = function(){
     headerLinksBox.style.width = "0px";
 }
 
+
+window.onscroll = function(){
+    let currScrollY = window.pageYOffset;
+
+    if( currScrollY > prevScrollY ){
+        header.style.top = "-20vh";
+    }else{
+        header.style.top = "0px";
+    }
+
+    prevScrollY = currScrollY;
+}
